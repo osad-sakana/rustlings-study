e57dd587e69006c44bbd7c4aea3dea38d6ce9e57
Yu Osada <osada@shisui.tokyo>
2026-06-05 10:28:18 +0900

    Add solutions for primitive types exercises and update quiz1 with pricing logic

--- exercises/quizzes/quiz1.rs
+++ exercises/quizzes/quiz1.rs
@@ -8,12 +8,30 @@
 // - However, if Mary buys more than 40 apples, the price of each apple in the
 // entire order is reduced to only 1 rustbuck!
 
+// 日本語版
+// メアリーはリンゴを買っています。リンゴの値段は以下のように計算されます。
+// - 1個のリンゴは2ラストバックスです。
+// - しかし、メアリーが40個以上のリンゴを買うと、注文全体のリンゴの値段が1ラストバックスに減ります！
+
 // TODO: Write a function that calculates the price of an order of apples given
 // the quantity bought.
 // fn calculate_price_of_apples(???) -> ??? { ??? }
 
+// TODO: 与えられたリンゴの数量に基づいて、リンゴの注文の価格を計算する関数を書いてください。
+
 fn main() {
     // You can optionally experiment here.
+}
+
+fn calculate_price_of_apples(quantity: u32) -> u32 {
+    const PRICE_PER_APPLE: u32 = 2;
+    const DISCOUNTED_PRICE_PER_APPLE: u32 = 1;
+
+    if quantity <= 40 {
+        quantity * PRICE_PER_APPLE
+    } else {
+        quantity * DISCOUNTED_PRICE_PER_APPLE
+    }
 }
 
 // Don't change the tests!
